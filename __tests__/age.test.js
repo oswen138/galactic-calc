936392dc0b09e29f58ddc165e6917d1e353025d6
ophelia swen <ophelia.swen@gmail.com>
2020-09-04 16:56:48 -0700

    at a loss on how to fix this but test logic statements wrote in order of what prompt was asking

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -22,42 +22,11 @@ describe('inputAge', () => {
     expect(venusAge).toEqual(2);
   });
   test("should determine how many years a user has left to live on each planet", function() {
-    let yearsLeft = (mercuryAge/88 - planetStay(Math.floor((this.age)/);                     //Age convert to planet age, then divide by year, 88 days = 1 year
-    expect(venusAge).toEqual(32);
+    let yearsLeft = ((Math.floor((this.age)/.24)/88));    //Age convert to planet age, then divide by planet year, ex-88 days = 1 year      
+    expect(yearsLeft).toEqual(5);
   });
   test("should return the number of years they have lived past the life expectancy", function() {
-    let venusAge = Math.floor((this.age)/11.86);
-    expect(venusAge).toEqual(32);
+    let yearsPast = (Math.floor((this.age)/.24)-100);     //300 years old in planet years - 100 (max life expectancy)
+    expect(yearsPast).toEqual(1150);
   });
-
-
 });
-//   test('should calcuate the age inputted in mercury years, function() => {
-//     let mercuryAge = math.floor(this.age/.24);
-//     expect(mercuryAge).toEqual();
-//     expect(triangle.side2).toEqual(4);
-//     expect(triangle.side3).toEqual(5);
-//   });
-
-//   test('should correctly determine whether three lengths are not a triangle', () => {
-//     const notTriangle = new Triangle(3,9,22);
-//     expect(notTriangle.checkType()).toEqual("not a triangle");
-//   });
-
-//   test('should correctly determine whether three lengths make an isosceles triangle', () => {
-//     const isocTriangle = new Triangle(5,5,7)
-//     expect(isocTriangle.checkType()).toEqual("isosceles triangle");
-//   });
-
-//   test('should correctly determine whether three lengths make an scalene triangle', () => {
-//     const scalTriangle = new Triangle(2,3,4)
-//     expect(scalTriangle.checkType()).toEqual("scalene triangle");
-//   });
-
-//   test('should correctly determine whether three lengths make an equilateral triangle', () => {
-//     const equiTriangle = new Triangle(5,5,5)
-//     expect(equiTriangle.checkType()).toEqual("equilateral triangle");
-//   });
-
-// });
-
