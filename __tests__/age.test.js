40f63981b8e25551fae037a1f02af301b88213a4
ophelia swen <ophelia.swen@gmail.com>
2020-09-04 16:32:47 -0700

    file isn't importing from my other files

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -1,40 +1,50 @@
-import Triangle from './../src/js/triangle.js';
+import {planetAge} from './../src/js/age.js';
 
-describe('Triangle', () => {
+describe('inputAge', function() => {
+  let inputAge;
 
-
-  this.mercury = math.floor(this.age/.24);
-  this.venus = math.floor(this.age/.62);
-  this.mars = math.floor(this.age/1.88);
-  this.jupiter = math.floor(this.age/11.86);
-
-
-
-  test('should correctly create a triangle object with three lengths', () => {
-    const triangle = new Triangle(2,4,5);
-    expect(triangle.side1).toEqual(2);
-    expect(triangle.side2).toEqual(4);
-    expect(triangle.side3).toEqual(5);
+  beforeEach(function(inputAge) {
+    let inputAge = [20, "Venus"];
+  });
+  
+  test("should calculate the inputted age in Venus years", function() {
+    let venusAge = Math.floor((this.age)/.62);
+    expect(venusAge).toEqual(32);
   });
 
-  test('should correctly determine whether three lengths are not a triangle', () => {
-    const notTriangle = new Triangle(3,9,22);
-    expect(notTriangle.checkType()).toEqual("not a triangle");
-  });
+//   test('should calcuate the age inputted in mercury years, function() => {
+//     let mercuryAge = math.floor(this.age/.24);
+//     expect(mercuryAge).toEqual();
+//     expect(triangle.side2).toEqual(4);
+//     expect(triangle.side3).toEqual(5);
+//   });
 
-  test('should correctly determine whether three lengths make an isosceles triangle', () => {
-    const isocTriangle = new Triangle(5,5,7)
-    expect(isocTriangle.checkType()).toEqual("isosceles triangle");
-  });
+//   test('should correctly determine whether three lengths are not a triangle', () => {
+//     const notTriangle = new Triangle(3,9,22);
+//     expect(notTriangle.checkType()).toEqual("not a triangle");
+//   });
 
-  test('should correctly determine whether three lengths make an scalene triangle', () => {
-    const scalTriangle = new Triangle(2,3,4)
-    expect(scalTriangle.checkType()).toEqual("scalene triangle");
-  });
+//   test('should correctly determine whether three lengths make an isosceles triangle', () => {
+//     const isocTriangle = new Triangle(5,5,7)
+//     expect(isocTriangle.checkType()).toEqual("isosceles triangle");
+//   });
 
-  test('should correctly determine whether three lengths make an equilateral triangle', () => {
-    const equiTriangle = new Triangle(5,5,5)
-    expect(equiTriangle.checkType()).toEqual("equilateral triangle");
-  });
+//   test('should correctly determine whether three lengths make an scalene triangle', () => {
+//     const scalTriangle = new Triangle(2,3,4)
+//     expect(scalTriangle.checkType()).toEqual("scalene triangle");
+//   });
 
-});
+//   test('should correctly determine whether three lengths make an equilateral triangle', () => {
+//     const equiTriangle = new Triangle(5,5,5)
+//     expect(equiTriangle.checkType()).toEqual("equilateral triangle");
+//   });
+
+// });
+
+
+
+
+// this.mercury = math.floor(this.age/.24);
+// this.venus = math.floor(this.age/.62);
+// this.mars = math.floor(this.age/1.88);
+// this.jupiter = math.floor(this.age/11.86);
