cfa490195760de0f399cc6652c41a3788f957a22
ophelia swen <ophelia.swen@gmail.com>
2020-09-04 17:01:30 -0700

    add whatever code I can

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -1,6 +1,6 @@
 import {planetAge} from './../src/js/age.js';
 
-describe('inputAge', () => {
+describe('planetAge', () => {
   
   test("should calculate the inputted age in Mercury years", function() {
     let mercuryAge = Math.floor((this.age)/.24);
@@ -20,13 +20,5 @@ describe('inputAge', () => {
   test("should calculate the inputted age in Jupiter years", function() {
     let venusAge = Math.floor((this.age)/11.86);
     expect(venusAge).toEqual(2);
-  });
-  test("should determine how many years a user has left to live on each planet", function() {
-    let yearsLeft = ((Math.floor((this.age)/.24)/88));    //Age convert to planet age, then divide by planet year, ex-88 days = 1 year      
-    expect(yearsLeft).toEqual(5);
-  });
-  test("should return the number of years they have lived past the life expectancy", function() {
-    let yearsPast = (Math.floor((this.age)/.24)-100);     //300 years old in planet years - 100 (max life expectancy)
-    expect(yearsPast).toEqual(1150);
   });
 });
--- __tests__/expectancy.test.js
+++ __tests__/expectancy.test.js
@@ -1,19 +1,14 @@
-import Rectangle from '../src/js/rectangle.js';
+import {lifeExpectancy} from '../src/js/expectancy.js';
 
-describe('Rectangle', () => {
+describe('yearsCount', () => {
 
-  let rectangle;
-
-  beforeEach(() => {
-    rectangle = new Rectangle(3,5);
+  test("should determine how many years a user has left to live on each planet", function() {
+    let yearsLeft = ((Math.floor((this.age)/.24)/88));    //Age convert to planet age, then divide by planet year, ex-88 days = 1 year      
+    expect(yearsLeft).toEqual(5);
   });
-
-  test('should correctly create a rectangle object using two sides', () => {
-    expect(rectangle.side1).toEqual(3);
-    expect(rectangle.side2).toEqual(5);
-  });
-
-  test('should correctly create a rectangle object using two sides', () => {
-    expect(rectangle.getArea()).toEqual(15);
+  test("should return the number of years they have lived past the life expectancy", function() {
+    let yearsPast = (Math.floor((this.age)/.24)-100);     //300 years old in planet years - 100 (max life expectancy)
+    expect(yearsPast).toEqual(1150);
   });
 });
+
