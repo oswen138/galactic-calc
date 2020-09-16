a7ed50e5ce4efbae6bf51a43c8c813a6f002b17e
ophelia swen <ophelia.swen@gmail.com>
2020-09-15 23:47:57 -0700

    follow advice from instructor and rework tests to pass, make sure names are correct and simplify logic

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -1,10 +1,13 @@
 import PlanetAge from '../src/js/age.js';
 
 describe('age on planet', () => {
-  let calcAge = new PlanetAge(15);
-
+  let calcAge;
+  beforeEach(() => {
+       calcAge = new PlanetAge(100, 15);
+  })
+  
   test('should calculate the age in Mercury years', () => {
-       expect(calcAge.ageMercury()).toEqual(62.5);
+       expect(calcAge.ageMercury()).toEqual(62);
   });
 
   test("should calculate the age in Venus years", () => {
@@ -12,13 +15,19 @@ describe('age on planet', () => {
   });
 
   test("should calculate the age in Mars years",() => {
-       expect(calcAge.ageMars()).toEqual(8);
+       expect(calcAge.ageMars()).toEqual(7);
   });
 
   test("should calculate the age in Jupiter years",() => {
        expect(calcAge.ageJupiter()).toEqual(1);
   });
-});
 
-
+test('should determine how many earth years a user has to live on each planet', ()=> {
+     calcAge.planetLeft();
+     expect(calcAge.mercLeft).toEqual(38);
+     expect(calcAge.venusLeft).toEqual(76);
+     expect(calcAge.marsLeft).toEqual(93);
+     expect(calcAge.jupLeft).toEqual(99);
+   });
+ });
 
